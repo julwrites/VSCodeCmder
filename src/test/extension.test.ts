c67a41e30f07764374a5c524ab0e5489941e493c
julwrites <julian.teh@outlook.com>
2019-06-22 10:32:56 +0800

    Updating tests to use mocha

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,12 +1,10 @@
-import assert = require('assert');
-
 //
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
 
 // // The module 'assert' provides assertion methods from node
-// import * as assert from 'assert';
+import * as assert from 'assert';
 
 // // You can import and use all API from the 'vscode' module
 // // as well as import your extension to test it
@@ -14,8 +12,8 @@ import assert = require('assert');
 // import * as codecmder from '../extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite('Navigation', () => {
-  test('Valid Workspace', () => {
+describe('Navigation', () => {
+  describe('Valid Workspace', () => {
     assert.equal(1, 2 - 1);
   });
 });
